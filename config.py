5db586db1eaeec9028dd55b871094101dbd667e3
Egor <80852474+Titanik9@users.noreply.github.com>
2022-10-21 14:55:49 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_API_TOKEN = "5338496544:AAEbhwlttI_D-MRJ6gvFQR7zshA18M9HxQw"
+WEATHER_API_KEY = "9b3dc32e46aa86bdf7e2b0b3998b57c6"
+
+СURRENT_WEATHER_API_CALL = (
+        'https://api.openweathermap.org/data/2.5/weather?'
+        'lat={latitude}&lon={longitude}&'
+        'appid=' + WEATHER_API_KEY + '&units=metric'
+)
